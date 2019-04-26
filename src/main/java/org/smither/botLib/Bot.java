109d40fac1770005d118bcd25d0af04241e36548
CSmither <callum@smither.org>
2019-04-27 00:47:50 +0100

    Chunk managing

--- src/main/java/org/smither/botLib/Bot.java
+++ src/main/java/org/smither/botLib/Bot.java
@@ -7,89 +7,78 @@ import com.github.steveice10.mc.protocol.data.message.Message;
 import com.github.steveice10.mc.protocol.data.message.TranslationMessage;
 import com.github.steveice10.mc.protocol.data.status.handler.ServerInfoHandler;
 import com.github.steveice10.mc.protocol.data.status.handler.ServerPingTimeHandler;
-import com.github.steveice10.mc.protocol.packet.ingame.client.ClientChatPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.server.ServerChatPacket;
-import com.github.steveice10.mc.protocol.packet.ingame.server.ServerJoinGamePacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerChunkDataPacket;
 import com.github.steveice10.mc.protocol.packet.login.server.LoginSuccessPacket;
 import com.github.steveice10.packetlib.Client;
 import com.github.steveice10.packetlib.event.session.DisconnectedEvent;
 import com.github.steveice10.packetlib.event.session.PacketReceivedEvent;
 import com.github.steveice10.packetlib.event.session.SessionAdapter;
 import com.github.steveice10.packetlib.tcp.TcpSessionFactory;
+import org.smither.botLib.environment.ChunkManager;
+
 import java.net.Proxy;
 import java.util.Arrays;
 
 public class Bot {
 
-    private static final Proxy PROXY = Proxy.NO_PROXY;
+	private static final Proxy PROXY = Proxy.NO_PROXY;
 
-    private static final Proxy AUTH_PROXY = Proxy.NO_PROXY;
+	private static final Proxy AUTH_PROXY = Proxy.NO_PROXY;
 
-    public Bot() {
-        status();
-        login();
-    }
+	private ChunkManager chunkManager = ChunkManager.getInstance();
 
-    private static void status() {
-        MinecraftProtocol protocol = new MinecraftProtocol(SubProtocol.STATUS);
-        Client client = new Client("smither.org", 25566, protocol, new TcpSessionFactory(PROXY));
-        client.getSession().setFlag(MinecraftConstants.AUTH_PROXY_KEY, AUTH_PROXY);
-        client.getSession()
-            .setFlag(MinecraftConstants.SERVER_INFO_HANDLER_KEY,
-                (ServerInfoHandler) (session, info) -> {
-                    System.out.println("Version: " + info.getVersionInfo().getVersionName() + ", "
-                        + info.getVersionInfo().getProtocolVersion());
-                    System.out.println(
-                        "Player Count: " + info.getPlayerInfo().getOnlinePlayers() + " / "
-                            + info.getPlayerInfo().getMaxPlayers());
-                    System.out.println(
-                        "Players: " + Arrays.toString(info.getPlayerInfo().getPlayers()));
-                    System.out.println("Description: " + info.getDescription().getFullText());
-                    System.out.println("Icon: " + info.getIcon());
-                });
-        client.getSession()
-            .setFlag(MinecraftConstants.SERVER_PING_TIME_HANDLER_KEY,
-                (ServerPingTimeHandler) (session, pingTime) -> System.out.println(
-                    "Server ping took " + pingTime + "ms"));
-        client.getSession().connect();
-        while (client.getSession().isConnected()) {
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	public Bot() {
+		status();
+		login();
+	}
 
-    private static void login() {
-        MinecraftProtocol protocol = null;
-        protocol = new MinecraftProtocol("bot");
-        Client client = new Client("smither.org", 25566, protocol, new TcpSessionFactory(PROXY));
-        client.getSession().setFlag(MinecraftConstants.AUTH_PROXY_KEY, AUTH_PROXY);
-        client.getSession().addListener(new SessionAdapter() {
-            @Override
-            public void packetReceived(PacketReceivedEvent event) {
-                System.out.println(event.getPacket().getClass().getCanonicalName());
-                if (event.getPacket() instanceof LoginSuccessPacket) {
-                } else if (event.getPacket() instanceof ServerChatPacket) {
-                    Message message = event.<ServerChatPacket>getPacket().getMessage();
-                    System.out.println("Received Message: " + message.getFullText());
-                    if (message instanceof TranslationMessage) {
-                        System.out.println("Received Translation Components: " + Arrays.toString(
-                            ((TranslationMessage) message).getTranslationParams()));
-                    }
-                }
-            }
+	private void status() {
+		MinecraftProtocol protocol = new MinecraftProtocol(SubProtocol.STATUS);
+		Client client = new Client("smither.org", 25566, protocol, new TcpSessionFactory(PROXY));
+		client.getSession().setFlag(MinecraftConstants.AUTH_PROXY_KEY, AUTH_PROXY);
+		client.getSession().setFlag(MinecraftConstants.SERVER_INFO_HANDLER_KEY,
+			(ServerInfoHandler) (session, info) -> {
+			System.out.println("Version: " + info.getVersionInfo().getVersionName() + ", " + info.getVersionInfo().getProtocolVersion());
+			System.out.println("Player Count: " + info.getPlayerInfo().getOnlinePlayers() + " / " + info.getPlayerInfo().getMaxPlayers());
+			System.out.println("Players: " + Arrays.toString(info.getPlayerInfo().getPlayers()));
+			System.out.println("Description: " + info.getDescription().getFullText());
+			System.out.println("Icon: " + info.getIcon());
+		});
+		client.getSession().setFlag(MinecraftConstants.SERVER_PING_TIME_HANDLER_KEY, (ServerPingTimeHandler) (session,
+		                                                                                                      pingTime) -> System.out.println("Server ping took " + pingTime + "ms"));
+		client.getSession().connect();
+	}
 
-            @Override
-            public void disconnected(DisconnectedEvent event) {
-                System.out.println(
-                    "Disconnected: " + Message.fromString(event.getReason()).getFullText());
-                if (event.getCause() != null) {
-                    event.getCause().printStackTrace();
-                }
-            }
-        });
-        client.getSession().connect();
-    }
+	private void login() {
+		MinecraftProtocol protocol = null;
+		protocol = new MinecraftProtocol("bot");
+		Client client = new Client("smither.org", 25566, protocol, new TcpSessionFactory(PROXY));
+		client.getSession().setFlag(MinecraftConstants.AUTH_PROXY_KEY, AUTH_PROXY);
+		client.getSession().addListener(new SessionAdapter() {
+			@Override
+			public void packetReceived(PacketReceivedEvent event) {
+				System.out.println(event.getPacket().getClass().getCanonicalName());
+				if (event.getPacket() instanceof LoginSuccessPacket) {
+				} else if (event.getPacket() instanceof ServerChatPacket) {
+					Message message = event.<ServerChatPacket>getPacket().getMessage();
+					System.out.println("Received Message: " + message.getFullText());
+					if (message instanceof TranslationMessage) {
+						System.out.println("Received Translation Components: " + Arrays.toString(((TranslationMessage) message).getTranslationParams()));
+					}
+				} else if (event.getPacket() instanceof ServerChunkDataPacket) {
+					chunkManager.updateChunk(event.getPacket());
+				}
+			}
+
+			@Override
+			public void disconnected(DisconnectedEvent event) {
+				System.out.println("Disconnected: " + Message.fromString(event.getReason()).getFullText());
+				if (event.getCause() != null) {
+					event.getCause().printStackTrace();
+				}
+			}
+		});
+		client.getSession().connect();
+	}
 }
--- src/main/java/org/smither/botLib/environment/ChunkManager.java
+++ src/main/java/org/smither/botLib/environment/ChunkManager.java
@@ -2,11 +2,22 @@ package org.smither.botLib.environment;
 
 import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerChunkDataPacket;
 
-/**
- * Description:
- */
 public class ChunkManager {
-    public void updateChunk(ServerChunkDataPacket chunkDataPacket){
-    }
+	private static ChunkManager instance;
+
+	private ChunkManager() {
+
+	}
+
+	public static ChunkManager getInstance() {
+		if (instance == null) {
+			instance = new ChunkManager();
+		}
+		return instance;
+	}
+
+	public void updateChunk(ServerChunkDataPacket chunkDataPacket) {
+		System.out.println(chunkDataPacket.getColumn().getTileEntities()[0]);
+	}
 
 }
